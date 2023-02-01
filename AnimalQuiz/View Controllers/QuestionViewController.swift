b36f53db7500b7b6ca0263a79d83401e18053149
Tatiana Mantrova <tatiana.mantrova@gmail.com>
2023-02-01 14:19:17 +0300

    добавлены экраны

---
+++ AnimalQuiz/View Controllers/QuestionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionViewController.swift
+//  AnimalQuiz
+//
+//  Created by Tatiana Mantrova on 01.02.2023.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
